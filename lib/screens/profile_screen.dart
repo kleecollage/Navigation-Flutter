f2d7b96bb714375cb9302fdc99bf04c17f635dd9
kleeCollage <klee.collage@gmail.com>
2026-02-24 20:12:23 -0600

    feat: Implement Alert Dialog

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -5,16 +5,43 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
+    return Scaffold(
       appBar: AppBar(title: Text('Profile Screen')),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pushNamed(context, '/configuration');
+            _showConfirmDialog(context);
           },
           child: const Text('Go to configuration'),
         ),
       ),
     );
   }
+}
+
+void _showConfirmDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        title: const Text('Confirmation'),
+        content: const Text('Are you sure you want go to configuration?'),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/configuration');
+            },
+            child: const Text('Confirm'),
+          ),
+        ],
+      );
+    },
+  );
 }
